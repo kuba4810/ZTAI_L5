bc6023cc9831b03c0654b5721af5bbe800eeeefb
xKubax <kuba__koziol@op.pl>
2019-01-11 18:02:06 +0100

    All exercises finished

---
+++ demo/src/main/java/kuba/apka/com/demo/post/Post.java
@@ -0,0 +1,73 @@
+package kuba.apka.com.demo.post;
+
+public class Post {
+    private long id;
+    private String title;
+    private String url;
+    private String content;
+
+    public Post(long id, String title, String url, String content){
+        this.id = id;
+        this.title = title;
+        this.url = url;
+        this.content = content;
+    }
+
+    /**
+     * @return the id
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @param title the title to set
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * @return the url
+     */
+    public String getUrl() {
+        return url;
+    }
+
+    /**
+     * @param url the url to set
+     */
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    /**
+     * @return the content
+     */
+    public String getContent() {
+        return content;
+    }
+
+    /**
+     * @param content the content to set
+     */
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+
+}
---
+++ demo/src/main/java/kuba/apka/com/demo/post/PostService.java
@@ -0,0 +1,61 @@
+package kuba.apka.com.demo.post;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Service;
+
+import ch.qos.logback.classic.spi.LoggerRemoteView;
+import java.util.List;
+
+@Service
+public class PostService{
+
+    private static ArrayList<Post> posts = new ArrayList<>();
+    private static int idCounter = 0;
+
+    static {
+        posts.add(new Post(++idCounter,
+         "title1",
+         "https://www.w3schools.com/w3css/img_lights.jpg",
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit,"+
+          "sed do eiusmod tempor incididunt ut labore et dolore magna" +
+          "aliqua. Ut enim ad minim veniam, quis nostrud exercitation" +
+          "ullamco laboris nisi ut aliquip ex ea commodo consequat" ));
+          posts.add(new Post(++idCounter,
+          "title1",
+          "https://www.w3schools.com/w3css/img_lights.jpg",
+           "Lorem ipsum dolor sit amet, consectetur adipiscing elit,"+
+           "sed do eiusmod tempor incididunt ut labore et dolore magna" +
+           "aliqua. Ut enim ad minim veniam, quis nostrud exercitation" +
+           "ullamco laboris nisi ut aliquip ex ea commodo consequat" ));
+           posts.add(new Post(++idCounter,
+           "title1",
+           "https://www.w3schools.com/w3css/img_lights.jpg",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit,"+
+            "sed do eiusmod tempor incididunt ut labore et dolore magna" +
+            "aliqua. Ut enim ad minim veniam, quis nostrud exercitation" +
+            "ullamco laboris nisi ut aliquip ex ea commodo consequat" ));
+            posts.add(new Post(++idCounter,
+            "title1",
+            "https://www.w3schools.com/w3css/img_lights.jpg",
+             "Lorem ipsum dolor sit amet, consectetur adipiscing elit,"+
+             "sed do eiusmod tempor incididunt ut labore et dolore magna" +
+             "aliqua. Ut enim ad minim veniam, quis nostrud exercitation" +
+             "ullamco laboris nisi ut aliquip ex ea commodo consequat" ));
+             posts.add(new Post(++idCounter,
+             "title1",
+             "https://www.w3schools.com/w3css/img_lights.jpg",
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit,"+
+              "sed do eiusmod tempor incididunt ut labore et dolore magna" +
+              "aliqua. Ut enim ad minim veniam, quis nostrud exercitation" +
+              "ullamco laboris nisi ut aliquip ex ea commodo consequat" ));
+    }
+
+    public List<Post> getAll(){
+        return posts;
+    }
+
+    public Post get(int id){
+        return posts.get(id);
+    }
+}
